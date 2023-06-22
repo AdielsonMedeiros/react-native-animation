1234c96045ee4cec60dfd6c1cea0c2097778c3f7
Adielson Medeiros <medeirosadielson@gmail.com>
2023-06-22 00:39:16 -0300

    Atualização do código

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, {useRef, useEffect} from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 
 export default function App() {
+
+  const larAnimada = useRef(new Animated.Value(150)).current;
+  const altAnimada = useRef(new Animated.Value(50)).current;
+
+  useEffect(()=>{
+    Animated.timing(larAnimada, {toValue: 300, duration: 5000, useNativeDriver: false}).start();
+    Animated.timing(altAnimada, {toValue: 150, duration: 5000, useNativeDriver: false}).start();
+
+
+  }, [])
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+
+    <Animated.View style={{width: larAnimada, height:altAnimada, backgroundColor: '#4169e1', justifyContent:'center', alignItems:'center'}}>
+      <Text style={{textAlign:'center', fontSize:22, color: '#fff'}}> loading... </Text>
+
+    </Animated.View>
     </View>
   );
 }
@@ -14,7 +31,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
