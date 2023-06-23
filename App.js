6c7ee9bfed35e33d298950a7cec2f0cfe92be1e4
Adielson Medeiros <medeirosadielson@gmail.com>
2023-06-23 01:07:58 -0300

    animacao em loop

--- App.js
+++ App.js
@@ -9,33 +9,34 @@ export default function App() {
   const opacidadeAnimada = useRef(new Animated.Value(1)).current;
 
   useEffect(()=>{
+  Animated.loop(
     Animated.sequence([
       Animated.timing(opacidadeAnimada, {
         toValue: 1,
-        duration: 2000,
+        duration: 1000,
         useNativeDriver: false
       }),
 
       Animated.parallel([
         Animated.timing(larAnimada, {
           toValue: 300,
-          duration: 2000,
+          duration: 1000,
           useNativeDriver: false
         }),
         Animated.timing(altAnimada, {
-          toValue: 300,
-          duration: 2000,
+          toValue: 50,
+          duration: 1000,
           useNativeDriver: false
         }),
       ]),
 
       Animated.timing(opacidadeAnimada, {
         toValue: 0,
-        duration: 2000,
+        duration: 1000,
         useNativeDriver: false
       })
 
-    ]).start();
+    ])).start();
 
   }, [])
 
@@ -45,7 +46,7 @@ export default function App() {
     <View style={styles.container}>
 
 
-    <Animated.View style={{width: larAnimada, height:altAnimada, backgroundColor: '#4169e1', justifyContent:'center', opacity: opacidadeAnimada}}>
+    <Animated.View style={{width: larAnimada, height:altAnimada, backgroundColor: '#4169e1', justifyContent:'center', opacity: opacidadeAnimada, borderRadius: 30}}>
       <Text style={{textAlign:'center', fontSize:22, color: '#fff'}}> loading... </Text>
 
     </Animated.View>
